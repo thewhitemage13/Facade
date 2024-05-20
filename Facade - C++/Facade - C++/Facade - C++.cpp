26d246f6ea4cae8125481116d6222c43d7bd39ca
Mukhammed Lolo <144810207+thewhitemage13@users.noreply.github.com>
2024-05-20 20:05:17 +0200

    Add files via upload

---
+++ Facade - C++/Facade - C++/Facade - C++.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+using namespace std;
+
+class ProviderCommunication {
+public:
+	void Receive() {
+		cout << "Product receipt\n";
+	}
+
+	void Payment() {
+		cout << "Payment to supplier with deduction of commission for sale of products" << "\n";
+	}
+};
+
+class Site {
+public:
+	void Placement() {
+		cout << "Website posting" << "\n";
+	}
+	void Del() {
+		cout << "Deletion from the site" << "\n";
+	}
+};
+
+class Database {
+public:
+	void Insert() {
+		cout << "Database entry" << "\n";
+	}
+	void Del() {
+		cout << "Database deletion" << "\n";
+	}
+};
+
+class MarketPlace {
+private:
+	ProviderCommunication providerCom;
+	Site site;
+	Database database;
+public:
+	void ProductReceipt() {
+		providerCom.Receive();
+		site.Placement();
+		database.Insert();
+	}
+
+	void ProductRelease() {
+		providerCom.Payment();
+		site.Del();
+		database.Del();
+	}
+};
+
+int main() {
+	MarketPlace marketplace;
+	marketplace.ProductReceipt();
+	cout << "=======================================\n";
+	marketplace.ProductRelease();
+}
